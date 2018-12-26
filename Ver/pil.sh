7f306993a2a24d60322e0bd3e04690bd03e7179f
Francois TARDIEU <fratardi@e2r11p22.42.fr>
2018-12-26 12:19:38 +0100

    ko

--- Ver/pil.sh
+++
@@ -1,1 +0,0 @@
-gcc  -g3 -Wall -Werror -Wextra libft/libft.a get_next_line.c ft_checkargs.c main.c
--- Ver/pilx.sh
+++
@@ -1,1 +0,0 @@
-gcc -Wall -Werror -Wextra get_next_line.c main.c ft_checkargs.c libft/libft.a -fsanitize=address
