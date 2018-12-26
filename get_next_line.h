85ac8f13533764a2ba6563fecc10b3ce44e48615
Francois TARDIEU <fratardi@e2r11p22.42.fr>
2018-12-26 05:44:10 +0100

    je deviendrais forta ca un jour

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: fratardi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 18:08:49 by fratardi          #+#    #+#             */
-/*   Updated: 2018/12/26 04:40:06 by fratardi         ###   ########.fr       */
+/*   Updated: 2018/12/26 05:43:47 by fratardi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 //reste 5 lignes 
@@ -16,7 +16,7 @@
 # include <fcntl.h>
 # include <unistd.h>
 # include <stdlib.h>
-# define BUFF_SIZE 6000
+# define BUFF_SIZE 5000
 
 
 //# define BUFF_SIZE 8381921
